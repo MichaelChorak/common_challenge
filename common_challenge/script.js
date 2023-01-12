9e3e6419faa7d370b4f0792bf2d53ddab93f7fcb
Michael Chorak <70633896+MichaelChorak@users.noreply.github.com>
2023-01-12 11:53:33 +0100

    Add files via upload
    Added a working search bar for the current added applications

---
+++ common_challenge/script.js
@@ -0,0 +1,51 @@
+const userCardTemplate = document.querySelector("[data-user-template]");
+const userCardContainer = document.querySelector("[data-user-cards-container]");
+const searchInput = document.querySelector("[data-search]");
+
+let users = [];
+
+
+const apps = [
+  {
+    title: "Instagram",
+    imgFile: "images/intro_image.png",
+    body: "Content text"
+  },
+  {
+    title: "Snapchat",
+    imgFile: "images/intro_image.png",  
+    body: "Content text"
+  },
+  {
+    title: "Facebook",
+    imgFile: "images/intro_image.png",  
+    body: "Content text"
+  }
+];
+
+
+
+
+users = apps.map((user) => {
+  const card = userCardTemplate.content.cloneNode(true).children[0];
+  const header = card.querySelector("[data-header");
+  const body = card.querySelector("[data-body");
+  const image = card.querySelector("[data-img]");
+  // const moreInfoBtn = card.querySelector("[data-info]");
+  header.textContent = user.title;
+  body.textContent = user.body;
+  image.src = user.imgFile;
+  // moreInfoBtn.textContent = product.title;
+  userCardContainer.append(card)
+  return { title: user.title, img: user.imgFile, body: user.body, element: card };
+})
+
+
+
+searchInput.addEventListener("input", (e) => {
+  const value = e.target.value.toLowerCase()
+  users.forEach((user) => {
+    const isVisible = user.title.toLowerCase().includes(value)
+    user.element.classList.toggle("hide", !isVisible);
+  });
+});
