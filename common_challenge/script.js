1e3284c57f471c679933611c62b31250a76afe39
MichaelChorak <Michael.chorak@hva.nl>
2023-01-20 21:23:56 +0100

    latest version, need to add the content function after

--- common_challenge/script.js
+++ common_challenge/script.js
@@ -1,50 +1,41 @@
 const userCardTemplate = document.querySelector("[data-user-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
 const searchInput = document.querySelector("[data-search]");
-
+const appInformationContainer = document.querySelector('.appInformationContainer');
 
 let users = [];
-
 
 const apps = [
   {
     title: "Facebook",
-    imgFile: "images/app-logos/facebook.png", 
-    
+    logo: "images/app-logos/facebook.png", 
   },
   {
     title: "Instagram",
-    imgFile: "images/app-logos/instagram.png"
-    
+    logo: "images/app-logos/instagram.png"
   },
   {
     title: "Snapchat",
-    imgFile: "images/app-logos/snapchat.png"
+    logo: "images/app-logos/snapchat.png"
   }
-  
 ];
 
 
 
-
 users = apps.map((user, index) => {
-  
+  user.id = index;
   const card = userCardTemplate.content.cloneNode(true).children[0];
   const header = card.querySelector("[data-header");
   const image = card.querySelector("[data-img]");
-  // const moreInfoBtn = card.querySelector("[data-info]");
-  header.textContent = user.title;
-  image.src = user.imgFile;
-  card.setAttribute("id", user.title);
-  card.setAttribute("id", user.title);
-  // card.setAttribute("class", "block");
-  user.id = index;
-  // moreInfoBtn.textContent = product.title;
-  userCardContainer.append(card)
-  return { title: user.title, img: user.imgFile, body: user.body, id: user.id, element: card };
-})
 
-console.log(users);
+  header.textContent = user.title;
+  image.src = user.logo;
+  card.setAttribute("id", user.title);
+  card.setAttribute("id", user.title);
+ 
+  userCardContainer.append(card)
+  return { title: user.title, img: user.logo, id: user.id, element: card };
+})
 
 
 searchInput.addEventListener("input", (e) => {
@@ -56,51 +47,70 @@ searchInput.addEventListener("input", (e) => {
 });
 
 
-
-
-
 const appInformation = [
   {
-    header: "images/app-logos/facebook.png",
+    logo: "images/app-logos/facebook.png",
     title: "Facebook",
+    body: "Hello G",
+  },
+  {
+    logo: "images/app-logos/instagram.png",
+    title: "Instagram",
     body: "Hello G"
   },
-  
+  {
+    logo: "images/app-logos/snapchat.png",
+    title: "Snapchat",
+    body: "Hello G"
+  },
 ];
 
+let appImage = document.querySelector("#app-logo");
+let imageFigure = document.querySelector('#image-figure');
+let figCaption = document.querySelector('#fig-caption');
 
-let appInformationContainer = document.querySelector('.appInformationContainer');
-  appInformation.map(function(app, index) {
-
-  let formTitles = appInformation;
-  let appForm = document.querySelectorAll('#app-form');
-  let appContent = document.querySelectorAll('#form-content');
-
-  let appInfoImage = document.querySelector("#app-logo");
-  let imageFigure = document.querySelector('#image-figure');
-  let figCaption = document.querySelector('#fig-caption');
-  app.id = index;
-  figCaption.textContent = app.title
-
-  appInfoImage.classList.add('imageSize')
-  imageFigure.classList.add('center-column-flex');
-})
+appImage.classList.add('imageSize')
+imageFigure.classList.add('center-column-flex');
 
 
-// appInformationContainer.classList.add('hidden');
-const facebookCard = document.querySelector('#Facebook');
 
+let appInfo = [];
+//selecting all app cards
+const appCard = document.querySelectorAll('.card');
+
+
+function showCurrentInformation(){
+
+  for(i = 0; i < appInformation.length; i++)
+    appInformation
+}
+
+//toggles the hidden class to display/hide the card container
 function showAppInfoContainer(){
   appInformationContainer.classList.toggle('hidden');
 }
-facebookCard.addEventListener('click', showAppInfoContainer);
 
+
+
+//loop through the apps to apply the open and close function to each of them
+  for(i = 0; i < appCard.length; i ++){
+
+
+
+    appCard[i].addEventListener('click', showAppInfoContainer);
+    appCard[i].addEventListener('click', showCurrentInformation);
+    
+  }
+
+// selecting the closing button
 closeBtn = document.querySelector('.close-btn');
 
+//if the screen is not hidden, then hide and vice versa
 function closeScreen() {
  if(appInformationContainer.classList.contains("hidden")){
       appInformationContainer.classList.remove("hidden");
    }
 }
 
+//applied this to the close button to close the container
 closeBtn.addEventListener('click', closeScreen);
